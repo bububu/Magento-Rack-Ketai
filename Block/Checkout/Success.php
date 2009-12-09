0ef58fa4a58970d7958e822a92467eef4f638c5c
hirokazu nishi <rack990@gmail.com>
2009-12-10 07:53:44 +0900

    change parent class

--- Block/Checkout/Success.php
+++ Block/Checkout/Success.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Multishipping checkout success information
- *
- * @category   Mage
- * @package    Mage_Checkout
- * @author      Magento Core Team <core@magentocommerce.com>
- */
-class Rack_Ketai_Block_Checkout_Success extends Mage_Checkout_Block_Multishipping_Success 
+class Rack_Ketai_Block_Checkout_Success extends Mage_Checkout_Block_Onepage_Success 
 {
 }
