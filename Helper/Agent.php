c25b161f88a0130874c092de33be7bc1037d02f0
hirokazu nishi <rack990@gmail.com>
2010-04-08 23:20:22 +0900

    modified:   Block/Checkout/Abstract.php 	modified:   Block/Checkout/Addresses.php 	modified:   Block/Checkout/Billing.php 	modified:   Block/Checkout/Cart.php 	modified:   Block/Checkout/Cart/Abstract.php 	modified:   Block/Checkout/Cart/Coupon.php 	modified:   Block/Checkout/Cart/Crosssell.php 	modified:   Block/Checkout/Cart/Item/Renderer.php 	modified:   Block/Checkout/Cart/Item/Renderer/Configurable.php 	modified:   Block/Checkout/Cart/Item/Renderer/Grouped.php 	modified:   Block/Checkout/Cart/Shipping.php 	modified:   Block/Checkout/Cart/Sidebar.php 	modified:   Block/Checkout/Cart/Totals.php 	modified:   Block/Checkout/Html/Radio.php 	modified:   Block/Checkout/Link.php 	modified:   Block/Checkout/Login.php 	modified:   Block/Checkout/Payment.php 	modified:   Block/Checkout/Payment/Info.php 	modified:   Block/Checkout/Payment/Methods.php 	modified:   Block/Checkout/Review.php 	modified:   Block/Checkout/Review/Info.php 	modified:   Block/Checkout/Shipping.php 	modified:   Block/Checkout/Shipping/Method.php 	modified:   Block/Checkout/Shipping/Method/Additional.php 	modified:   Block/Checkout/Shipping/Method/Available.php 	modified:   Block/Checkout/State.php 	modified:   Block/Checkout/Success.php 	modified:   Helper/Agent.php 	modified:   Helper/Cart.php 	modified:   Helper/Url.php 	modified:   Model/Type/Abstract.php 	modified:   Model/Type/Checkout.php 	modified:   Model/Type/Checkout/State.php 	modified:   Model/Type/Ketai.php 	modified:   Model/Type/Standard.php 	modified:   controllers/CartController.php 	modified:   controllers/CheckoutController.php

--- Helper/Agent.php
+++ Helper/Agent.php
@@ -2,56 +2,66 @@
 class Rack_Ketai_Helper_Agent
 {
     protected $_agent;
+    protected $_isAdmin = false;
 
     public function __construct()
     {
-        $error = error_reporting(E_ALL);
-        $include_path = get_include_path();
-        set_include_path($include_path . PS . BP . DS . 'lib/PEAR');
-        require_once('Net/UserAgent/Mobile.php');
-        
-        $this->_agent = Net_UserAgent_Mobile::singleton();
+        $uri = Mage::app()->getRequest();
+        if(preg_match("/extensions_custom/i", $uri->getRequestUri())) {
+            $this->_isAdmin = true;
+        }
+        if(!$this->_isAdmin){
+            $error = error_reporting(E_ALL);
+            $include_path = get_include_path();
+            set_include_path($include_path . PS . BP . DS . 'lib/PEAR');
+            require_once('Net/UserAgent/Mobile.php');
+            $this->_agent = Net_UserAgent_Mobile::singleton();
+        }
     }
 
     public function getCookieEnable()
     {
         $cookie = true;
-
-        switch(true)
-        {
-        case ($this->_agent->isDoCoMo()) :
-            $cookie = $this->dispatchBrowser();
-            break;
-        case ($this->_agent->isEzweb()) :
-        case ($this->_agent->isSoftbank()) :
-        default :
-            $cookie = true;
-            break;
+        if(!$this->_isAdmin){
+            switch(true)
+            {
+            case ($this->_agent->isDoCoMo()) :
+                $cookie = $this->dispatchBrowser();
+                break;
+            case ($this->_agent->isEzweb()) :
+            case ($this->_agent->isSoftbank()) :
+            default :
+                $cookie = true;
+                break;
+            }
         }
-
         return $cookie;
     }
 
     public function isMobile()
     {
-        switch(true)
-        {
-            case ($this->_agent->isDoCoMo()) :
-            case ($this->_agent->isEzweb()) :
-            case ($this->_agent->isSoftbank()) :
-                return true;
-            default :
-                return false;
+        if(!$this->_isAdmin){
+            switch(true)
+            {
+                case ($this->_agent->isDoCoMo()) :
+                case ($this->_agent->isEzweb()) :
+                case ($this->_agent->isSoftbank()) :
+                    return true;
+                default :
+                    return false;
+            }
         }
+        return false;
     }
 
     protected function dispatchBrowser()
     {
-        if($this->_agent->getBrowserVersion() == "2.0")
-        {
-            return true;
+        if(!$this->_isAdmin){
+            if($this->_agent->getBrowserVersion() == "2.0")
+            {
+                return true;
+            }
         }
-
         return false;
     }
 }
